b1957697f12eea112467564c5b46a512de750d52
wzk <332684470@qq.com>
2023-01-23 19:30:37 +0800

    update

--- 02同步和异步/08手写Promise ver3.0.js
+++ 02同步和异步/08手写Promise ver3.0.js
@@ -7,7 +7,7 @@ const PROMISE_STATE = {
 class MyPromise {
     #result;
     #state = PROMISE_STATE.PENDING;//记录Promise状态 pending fulfilled rejected
-    #callbacks=[];//存储回调函数,由于回调函数可能有多个,所以我们用数组来存储
+    #callbacks = [];//存储回调函数,由于回调函数可能有多个,所以我们用数组来存储
     constructor(executor) {
         executor(this.#resolve.bind(this), this.#reject.bind(this));
     }
@@ -17,7 +17,7 @@ class MyPromise {
             this.#state = PROMISE_STATE.FULFILLED;
             queueMicrotask(() => {
                 // 调用callbacks中的所有函数
-                this.#callbacks.forEach(cb=>{
+                this.#callbacks.forEach(cb => {
                     cb();
                 })
             })
@@ -36,19 +36,22 @@ class MyPromise {
         else { return; }
     }
     then(onFulfilled, onRejected) {
-        if (this.#state === PROMISE_STATE.PENDING) {
-            queueMicrotask(() => {
-                this.#callbacks.push(()=>{
-                    onFulfilled(this.#result);
-                });
-            })
-        }
-        else if (this.#state === PROMISE_STATE.FULFILLED) {
-            onFulfilled(this.#result);
-        }
-        else if (this.#state === PROMISE_STATE.REJECTED) {
-            onRejected(this.#result);
-        }
+        return new MyPromise((resolve,reject)=>{
+            if (this.#state === PROMISE_STATE.PENDING) {
+                queueMicrotask(() => {
+                    this.#callbacks.push(() => {
+                        onFulfilled(this.#result);
+                    });
+                })
+            }
+            else if (this.#state === PROMISE_STATE.FULFILLED) {
+                onFulfilled(this.#result);
+            }
+            else if (this.#state === PROMISE_STATE.REJECTED) {
+                onRejected(this.#result);
+            }
+        });
+
     }
 }
 
@@ -64,20 +67,8 @@ console.log(mp1);
 mp1.then((result) => {
     console.log(result);//白金之星
 })
+    .then((result) => {
+        console.log(result);//白金之星
+    })
 
 
-const p = Promise.resolve('孙悟空');
-p.then(r => console.log('第一次', r));//第一次 孙悟空
-p.then(r => console.log('第二次', r));//第二次 孙悟空
-p.then(r => console.log('第三次', r));//第三次 孙悟空
-
-mp1.then((result) => {
-    console.log('1', result);//白金之星
-})
-mp1.then((result) => {
-    console.log('2', result);//白金之星
-})
-mp1.then((result) => {
-    console.log('3', result);//白金之星
-})
-//只读了 3 白金之星
